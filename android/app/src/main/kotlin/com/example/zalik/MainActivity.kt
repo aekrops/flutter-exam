55909a46d2d3352a5f5264f346f88cc9acac3d4e
Sviatoslav Trostynskyi <svyatoslavtrostynski24@gmail.com>
2021-12-16 19:12:48 +0200

    added not working app :(
    Ability to generate array with random length

---
+++ android/app/src/main/kotlin/com/example/zalik/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zalik
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:zalik/pages/home.dart';
+import 'constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Zalik :)',
+      theme: ThemeData(
+        scaffoldBackgroundColor: primaryColor,
+        primaryColor: primaryColor,
+      ),
+      home: const HomeScreen(),
+
+    );
+  }
+}
